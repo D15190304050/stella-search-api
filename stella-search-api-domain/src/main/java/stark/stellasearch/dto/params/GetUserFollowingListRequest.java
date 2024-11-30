06ca3e7139bfbee52b34f4b66127f56f5a148d76
Lorleen <luling_g163@163.com>
2024-11-30 19:40:41 +0800

    Modify the annotation of '@EqualsAndHashCode(callSuper = true)'.

--- stella-search-api-domain/src/main/java/stark/stellasearch/dto/params/GetUserFollowingListRequest.java
+++ stella-search-api-domain/src/main/java/stark/stellasearch/dto/params/GetUserFollowingListRequest.java
@@ -1,8 +1,10 @@
 package stark.stellasearch.dto.params;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class GetUserFollowingListRequest extends PaginationRequestParam
 {
     private String username;
--- stella-search-api-domain/src/main/java/stark/stellasearch/dto/params/ShowFavoritesByPlaylistRequest.java
+++ stella-search-api-domain/src/main/java/stark/stellasearch/dto/params/ShowFavoritesByPlaylistRequest.java
@@ -3,8 +3,10 @@ package stark.stellasearch.dto.params;
 import lombok.Data;
 
 import jakarta.validation.constraints.Min;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class ShowFavoritesByPlaylistRequest extends PaginationRequestParam
 {
     @Min(value = 1, message = "Minimum playlist ID is 1.")
